d82a397c293647dc6a916a80c05da7ee844ee3b6
arvindeva <arvindeva@gmail.com>
2021-10-21 17:46:38 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+
+  }
+
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+          <span role="img" aria-label="Wave Emoji">👋</span> Hey there!
+        </div>
+
+        <div className="bio">
+          <p>I am R'veen and I'm a frontend developer!</p>
+          <p>Connect your Ethereum wallet and wave at me!</p>
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
